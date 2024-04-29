2760fdb9a7c2346c41491d0eb6ff670c69ae1b58
Nahom Tegaude <nahomteguade@gmail.com>
2024-04-29 16:01:34 +0300

    Setup Structure

--- object-file/src/App.java
+++ object-file/src/App.java
@@ -1,12 +1,55 @@
+import java.io.File;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
     public void start (Stage primaryStage) {
-        Label label = new Label();
-    }
+        Label nameLabel = new Label("Name");
+        Label idLabel = new Label("Id Number");
+        Label addressLabel = new Label("Address");
 
+        TextField nameText = new TextField();
+        TextField idText = new TextField();
+        TextField addressText = new TextField();
+
+        Button regButton = new Button();
+
+        VBox vbox1 = new VBox();
+        vbox1.getChildren().addAll(nameLabel, nameText, idLabel, idText);
+
+        VBox vbox2 = new VBox();
+        vbox2.getChildren().addAll(addressLabel, addressText, regButton);
+
+        regButton.setOnAction(e -> {
+            String name = nameText.getText();
+            String address = addressText.getText();
+            String id = idText.getText();
+
+            User user = new User(id, name, address);
+            writeIntoFIle(user);
+
+        });
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(vbox1, vbox2);
+
+        Scene scene = new Scene(stackPane, 300, 700);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public void writeIntoFIle (User user) {
+        File file = new File("data.txt");
+
+    }
     public static void main(String[] args) throws Exception {
         launch(args);
     }
---
+++ object-file/src/User.java
@@ -0,0 +1,9 @@
+public class User {
+    String name, address, id;
+
+    User (String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+}
