6870b6ae2a89bdfd9e52209263ec52ead7b1f655
Nahom Tegaude <nahomteguade@gmail.com>
2024-04-29 15:42:48 +0300

    Object To file

---
+++ object-file/src/App.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public void start (Stage primaryStage) {
+        Label label = new Label();
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
