c2fb1a4290187a8c21b3aa1824372042242bdd94
Nahom Tegaude <nahomteguade@gmail.com>
2024-06-06 13:52:03 +0300

    First Connection

--- DBCon/src/App.java
+++ DBCon/src/App.java
@@ -1,5 +1,14 @@
+import java.sql.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String sql = "select customername from customer where customer_id = 2";
+        String url = "jdbc:mysql//localhost:3306/crowd_funding";
+        String username = "root";
+        String password = "";
+
+        Connection conn = DriverManager.getConnection(url, username, password);
+        Statement stmt = conn.createStatement();
+        stmt.executeQuery(sql);
     }
 }
