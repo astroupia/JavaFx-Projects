d54e8364659bf6e84b7f1360ba9df0e7bff4d39c
Nahom Tegaude <nahomteguade@gmail.com>
2024-06-06 14:42:09 +0300

    Result Output Trial

--- DBCon/src/App.java
+++ DBCon/src/App.java
@@ -2,13 +2,17 @@ import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String sql = "select customername from customer where customer_id = 2";
-        String url = "jdbc:mysql//localhost:3306/crowd_funding";
+        String sql = "SELECT customer_customername FROM customer";
+        String url = "jdbc:mysql://localhost:3306/crowd_funding";
         String username = "root";
         String password = "";
 
         Connection conn = DriverManager.getConnection(url, username, password);
+        Class.forName("com.mysql.cj.jdbc.Driver");
         Statement stmt = conn.createStatement();
-        stmt.executeQuery(sql);
+        ResultSet result = stmt.executeQuery(sql);
+        result.next();
+        result.getString(1);
+        System.out.println(result);
     }
 }
